9d60059c84f42fcc9822fa53327f52b89f5d6b69
JamesFHwang <jamesfhwang@outlook.com>
2018-10-17 20:18:45 -0700

    Update Assignment7.cpp

--- Assignment7.cpp
+++ Assignment7.cpp
@@ -1,5 +1,11 @@
-#include<iostream>
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
 using namespace std;
+
 
 void towerOfHanoi(int n, char Source, char Auxillary, char Destination);
 
@@ -12,16 +18,19 @@ int main()
 
 	towerOfHanoi(n, 'A', 'B', 'C');
 
+	
+
 }
 
 void towerOfHanoi(int n, char Source, char Auxillary, char Destination)
 {
 	if (n == 1)
 	{
-		cout << "Move Disk " << Source << " to " << Destination << endl;
+		cout << "Move Disk " << n << " from " << Source << " to " << Destination << endl;
 		return;
 	}
 
 	towerOfHanoi(n - 1, Source, Destination, Auxillary);
-	cout << "Move disk " << n << " from " << Auxillary << "to" << Destination << endl;
+	cout << "Move disk " << n << " from " << Auxillary << " to " << Destination << endl;
 	towerOfHanoi(n - 1, Auxillary, Source, Destination);
+}
